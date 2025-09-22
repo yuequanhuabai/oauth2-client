cb903c6043c2053d19167796b218f00f4fd17bc2
yuequanhuabai <yuequanhuabai@qq.com>
2025-09-22 23:31:21 +0800

    20250922

--- src/main/java/com/t/b/oauth2client/controller/OAuth2ClientController.java
+++ src/main/java/com/t/b/oauth2client/controller/OAuth2ClientController.java
@@ -3,8 +3,8 @@ package com.t.b.oauth2client.controller;
 
 import com.t.b.oauth2client.service.OAuth2Service;
 import jakarta.servlet.http.HttpSession;
-import lombok.Value;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
